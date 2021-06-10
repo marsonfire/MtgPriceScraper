af57779a7d5d9a064f113c330acb699ec38d2ab2
marsonfire <awmarsden487@gmail.com>
2021-06-10 14:32:39 -0400

    Cleaned up, consolidated code. Works with SCG and CK

--- SpiderRunner.py
+++ SpiderRunner.py
@@ -1,14 +1,8 @@
-import EnduranceTcgPlayerSpider, EnduranceCardKingdomSpider, EnduranceScgSpider,TestSpiders
-
+import MtgSpiders
 
 #https://towardsdatascience.com/tutorial-scrape-100-headlines-in-seconds-with-23-lines-of-python-14047deb1a98
 #https://towardsdatascience.com/web-scraping-with-scrapy-8071fd627051
 #scrapy runspider EnduranceTcgPlayerSpider.py -o test.csv
 
-#broken cuz tcgplayer changed thier site
-#EnduranceTcgPlayerSpider.EnduranceTcgPlayerRunner.run_spider()
-
-# EnduranceCardKingdomSpider.EnduranceCardKingdomRunner.run_spider()
-# EnduranceScgSpider.EnduranceScgRunner.run_spider()
-
-TestSpiders.Runner.run_spider()
+#TCG Player changed their site, so cannot get it now. Will figure out eventually
+MtgSpiders.Runner.run_spiders()
