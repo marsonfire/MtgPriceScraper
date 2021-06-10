eaf655c860315f1b43050963364285c663cff271
marsonfire <awmarsden487@gmail.com>
2021-06-10 14:28:12 -0400

    CK and SCG spiders work

--- EnduranceCardKingdomSpider.py
+++ EnduranceCardKingdomSpider.py
@@ -3,26 +3,25 @@ from csv import writer
 from datetime import datetime
 from scrapy.crawler import CrawlerProcess
 
-class EnduranceTcgPlayerSpider(scrapy.Spider):
-    name = "enduranceTcgPlayerSpider"
-    start_urls = ['https://shop.tcgplayer.com/magic/modern-horizons-2/endurance?xid=a5beae949-191c-4d02-9309-692dc57d1b5a']
+class EnduranceCardKingdomSpider(scrapy.Spider):
+    name = "enduranceCardKingdomSpider"
+    start_urls = ['https://www.cardkingdom.com/mtg/modern-horizons-2/endurance']
 
     def parse(self, response):
-        enduranceTcgPlayerPrice = [datetime.now().strftime("%Y-%m-%d %H:%M"), response.css('.price-point__data::text').get()]
+        endurance_ck_price = [datetime.now().strftime("%Y-%m-%d %H:%M"), response.xpath("//span[@class='stylePrice']/text()").get().strip()]
         # Open file in append mode
         with open('test.csv', 'a+', newline='') as write_obj:
             # Create a writer object from csv module
             csv_writer = writer(write_obj)
             # Add contents of list as last row in the csv file
-            csv_writer.writerow(enduranceTcgPlayerPrice)
+            csv_writer.writerow(endurance_ck_price)
 
-class EnduranceTcgPlayerRunner():
+class EnduranceCardKingdomRunner():
     def run_spider():
         process = CrawlerProcess({
             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
         })
-        process.crawl(EnduranceTcgPlayerSpider)
-        process.start() # the script will block here until the crawling is finished
+        process.crawl(EnduranceCardKingdomSpider)
 
 
 
--- SpiderRunner.py
+++ SpiderRunner.py
@@ -1,3 +1,14 @@
-import EnduranceTcgPlayerSpider
+import EnduranceTcgPlayerSpider, EnduranceCardKingdomSpider, EnduranceScgSpider,TestSpiders
 
-EnduranceTcgPlayerSpider.EnduranceTcgPlayerRunner.run_spider()
+
+#https://towardsdatascience.com/tutorial-scrape-100-headlines-in-seconds-with-23-lines-of-python-14047deb1a98
+#https://towardsdatascience.com/web-scraping-with-scrapy-8071fd627051
+#scrapy runspider EnduranceTcgPlayerSpider.py -o test.csv
+
+#broken cuz tcgplayer changed thier site
+#EnduranceTcgPlayerSpider.EnduranceTcgPlayerRunner.run_spider()
+
+# EnduranceCardKingdomSpider.EnduranceCardKingdomRunner.run_spider()
+# EnduranceScgSpider.EnduranceScgRunner.run_spider()
+
+TestSpiders.Runner.run_spider()
