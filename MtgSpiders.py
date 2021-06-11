29ef69b13e64384058e5dafa2aac7560dbea6f5d
marsonfire <awmarsden487@gmail.com>
2021-06-10 21:33:37 -0400

    Added more cards and organized better

--- MtgSpiders.py
+++ MtgSpiders.py
@@ -3,7 +3,12 @@ from csv import writer
 from datetime import datetime
 from scrapy.crawler import CrawlerProcess
 
-# region Endurance Spiders
+ck = "Card Kingdom"
+scg = "Star City Games"
+tcg = "Tcg Player Market"
+ch = "Cardhoarder"
+
+# region Spiders
 
 # Endurance Card Kingdom 
 class EnduranceCardKingdomSpider(scrapy.Spider):
@@ -11,8 +16,8 @@ class EnduranceCardKingdomSpider(scrapy.Spider):
     start_urls = ['https://www.cardkingdom.com/mtg/modern-horizons-2/endurance']
 
     def parse(self, response):
-        endurance_ck_price = SpiderHelpers.create_list_of_values('Card Kingdom', response.xpath("//span[@class='stylePrice']/text()").get().strip())
-        SpiderHelpers.append_to_csv(endurance_ck_price)
+        endurance_ck_price = SpiderHelpers.create_list_of_values(ck, response.xpath("//span[@class='stylePrice']/text()").get().strip())
+        SpiderHelpers.append_to_csv('endurance', endurance_ck_price)
 
 # Endurance Star City Games
 class EnduranceScgSpider(scrapy.Spider):
@@ -20,18 +25,63 @@ class EnduranceScgSpider(scrapy.Spider):
     start_urls = ['https://starcitygames.com/endurance-sgl-mtg-mh2-157-enn/?sku=SGL-MTG-MH2-157-ENN1']
 
     def parse(self, response):
-        endurance_scg_price = SpiderHelpers.create_list_of_values('Star City Games', response.xpath("//span[@class='price price--withoutTax']/text()").get())
-        SpiderHelpers.append_to_csv(endurance_scg_price)
+        endurance_scg_price = SpiderHelpers.create_list_of_values(scg, response.xpath("//span[@class='price price--withoutTax']/text()").get())
+        SpiderHelpers.append_to_csv('endurance', endurance_scg_price)
 
+#Endurance TCG Player and Cardhoarder
 class EnduranceGoldfishSpider(scrapy.Spider):
     name = "enduranceGoldfishSpider"
     start_urls = ['https://www.mtggoldfish.com/price/Modern+Horizons+2/Endurance#paper']
 
     def parse(self, response):
-        endurance_tcg_price = SpiderHelpers.create_list_of_values('Tcg Player Market', str(response.xpath("//span[@class='btn-shop-price']/text()")[2].get()).strip())
-        endurance_ch_price = SpiderHelpers.create_list_of_values('Cardhoarder', str(response.xpath("//span[@class='btn-shop-price']/text()")[4].get()).strip())
-        SpiderHelpers.append_to_csv(endurance_tcg_price)
-        SpiderHelpers.append_to_csv(endurance_ch_price)
+        endurance_tcg_price = SpiderHelpers.create_list_of_values(tcg, str(response.xpath("//span[@class='btn-shop-price']/text()")[2].get()).strip())
+        endurance_ch_price = SpiderHelpers.create_list_of_values(ch, str(response.xpath("//span[@class='btn-shop-price']/text()")[4].get()).strip())
+        SpiderHelpers.append_to_csv('endurance', endurance_tcg_price)
+        SpiderHelpers.append_to_csv('endurance', endurance_ch_price)
+
+# Polluted Delta TCG Player
+class PollutedDeltaGoldfishSpider(scrapy.Spider):
+    name = "pdGoldfishSpider"
+    start_urls = ['https://www.mtggoldfish.com/price/Khans+of+Tarkir/Polluted+Delta#online']
+
+    def parse(self, response):
+        pd_tcg_price = SpiderHelpers.create_list_of_values(tcg, str(response.xpath("//span[@class='btn-shop-price']/text()")[3].get()).strip())
+        SpiderHelpers.append_to_csv('pollutedDelta', pd_tcg_price)
+
+#Polluted Delta Card Kingom
+class PollutedDeltaCardKingdomSpider(scrapy.Spider):
+    name = "pdCkSpider"
+    start_urls = ['https://www.cardkingdom.com/mtg/khans-of-tarkir/polluted-delta']
+
+    def parse(self, response):
+        pd_ck_price = SpiderHelpers.create_list_of_values(ck, response.xpath("//span[@class='stylePrice']/text()").get().strip())
+        SpiderHelpers.append_to_csv('pollutedDelta', pd_ck_price)
+
+# Force of Will TCG Player
+class ForceOfWillGoldfishSpider(scrapy.Spider):
+    name = "fowSpider"
+    start_urls = ['https://www.mtggoldfish.com/price/Alliances/Force+of+Will#paper']
+
+    def parse(self, response):
+        fow_tcg_price = SpiderHelpers.create_list_of_values(tcg, str(response.xpath("//span[@class='btn-shop-price']/text()")[3].get()).strip())
+        SpiderHelpers.append_to_csv('forceOfWill', fow_tcg_price)
+
+# Force of Negation TCG Player
+class ForceOfNegationGoldfishSpider(scrapy.Spider):
+    name = "fonSpider"
+    start_urls = ['https://www.mtggoldfish.com/price/Modern+Horizons/Force+of+Negation#online']
+
+    def parse(self, response):
+        fon_tcg_price = SpiderHelpers.create_list_of_values(tcg, str(response.xpath("//span[@class='btn-shop-price']/text()")[3].get()).strip())
+        SpiderHelpers.append_to_csv('forceOfNegation', fon_tcg_price)
+
+class VerdantCatacombsSpider(scrapy.Spider):
+    name = "vcSpider"
+    start_urls = ['https://www.mtggoldfish.com/price/Modern+Horizons+2/Verdant+Catacombs#online']
+
+    def parse(self, response):
+        vc_tcg_price = SpiderHelpers.create_list_of_values(tcg, str(response.xpath("//span[@class='btn-shop-price']/text()")[2].get()).strip())
+        SpiderHelpers.append_to_csv('verdantCatacombs', vc_tcg_price)
 
 # endregion 
 
@@ -43,9 +93,9 @@ class SpiderHelpers():
         return [datetime.now().strftime("%Y-%m-%d %H:%M"), site, xpath] 
 
     # Open the CSV and append the new row to it
-    def append_to_csv(new_row):
+    def append_to_csv(csv_name, new_row):
         # Open file in append mode
-        with open('test.csv', 'a+', newline='') as write_obj:
+        with open("csvData/ " + csv_name + '.csv', 'a+', newline='') as write_obj:
             # Create a writer object from csv module
             csv_writer = writer(write_obj)
             # Add contents of list as last row in the csv file
@@ -59,4 +109,9 @@ class SpiderHelpers():
         process.crawl(EnduranceCardKingdomSpider)
         process.crawl(EnduranceScgSpider)
         process.crawl(EnduranceGoldfishSpider)
+        process.crawl(PollutedDeltaGoldfishSpider)
+        process.crawl(PollutedDeltaCardKingdomSpider)
+        process.crawl(ForceOfWillGoldfishSpider)
+        process.crawl(ForceOfNegationGoldfishSpider)
+        process.crawl(VerdantCatacombsSpider)
         process.start()
